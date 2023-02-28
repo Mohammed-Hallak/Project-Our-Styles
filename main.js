2ec1f49eff019851b83c48e331ed9ba9f7b4f640
Mohammed-Hallak <121720019+Mohammed-Hallak@users.noreply.github.com>
2023-02-28 21:23:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,46 @@ let clickableButton = document.querySelector(".button-for-links");
 clickableButton.onclick = function () {
   clickedButton.classList.toggle("cliked");
 };
+
+let getCards = document.querySelectorAll(".Our-Testimonials .test-cards .card");
+let getCardsOne = document.querySelectorAll(
+  ".Our-Testimonials .test-cards .card.one"
+);
+let getCardsTwo = document.querySelectorAll(
+  ".Our-Testimonials .test-cards .card.two"
+);
+
+let getCardsThree = document.querySelectorAll(
+  ".Our-Testimonials .test-cards .card.three"
+);
+let getCircles = document.querySelectorAll(".Our-Testimonials .circles li");
+getCircles.forEach((li) => {
+  li.addEventListener("click", (lis) => {
+    getCircles.forEach((lolo) => {
+      lolo.classList.remove("active");
+    });
+    li.classList.add("active");
+
+    getCardsOne.forEach((cardOne) => {
+      if (cardOne.classList[1] === li.getAttribute("data-number")) {
+        cardOne.style.display = "block";
+      } else {
+        cardOne.style.display = "none";
+      }
+    });
+    getCardsTwo.forEach((cardTwo) => {
+      if (cardTwo.classList[1] === li.getAttribute("data-number")) {
+        cardTwo.style.display = "block";
+      } else {
+        cardTwo.style.display = "none";
+      }
+    });
+    getCardsThree.forEach((cardThree) => {
+      if (cardThree.classList[1] === li.getAttribute("data-number")) {
+        cardThree.style.display = "block";
+      } else {
+        cardThree.style.display = "none";
+      }
+    });
+  });
+});
